1637d27bfaa3e0cf95fa9c17e394b50d6afa3d9d
Falilou66 <fadelvlt29@gmail.com>
2026-07-15 03:05:32 +0000

    first commit

---
+++ samabank-backend/src/main/java/sn/samabank/samabank_backend/SamabankBackendApplication.java
@@ -0,0 +1,13 @@
+package sn.samabank.samabank_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamabankBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamabankBackendApplication.class, args);
+	}
+
+}
---
+++ samabank-backend/src/test/java/sn/samabank/samabank_backend/SamabankBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.samabank.samabank_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamabankBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
